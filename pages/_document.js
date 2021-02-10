2db333cc24ccb4a869b82be773bcd418b3f37e33
IvanJoh <ivanjohannes14@gmail.com>
2021-02-10 21:10:29 +0200

    mistake on the analytics code

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,7 @@ class MyDocument extends Document {
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', '${+process.env.GA_PROPERTY_ID}');
+                    gtag('config', '${process.env.GA_PROPERTY_ID}');
                     `,
                 }}
             />
