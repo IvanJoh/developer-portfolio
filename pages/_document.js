0fb4ba9b02610aea2f4a3b2d567c80f23ae2618f
IvanJoh <ivanjohannes14@gmail.com>
2021-02-10 21:34:23 +0200

    trying something new

--- pages/_document.js
+++ pages/_document.js
@@ -9,23 +9,6 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-            <script 
-                async 
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_PROPERTY_ID}`}
-            />
-
-            <script
-                dangerouslySetInnerHTML={{
-                __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${process.env.GA_PROPERTY_ID}');
-                    `,
-                }}
-            />
-        </Head>
         <body>
           <Main />
           <NextScript />
